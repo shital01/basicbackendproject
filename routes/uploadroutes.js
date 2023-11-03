9ea1d141cb6af5b77345553e912e6a267a78dbe2
Sheetal Godara <sheetalgodara@Jaideeps-MacBook-Pro.local>
2023-11-03 16:48:42 +0530

    mulitple url api

--- routes/uploadroutes.js
+++ routes/uploadroutes.js
@@ -24,4 +24,37 @@ router.get('/',auth,async(req,res)=>{
 			res.send({key,url});
 		})
 	})
+
+
+
+router.get('/multiple', auth, async (req, res) => {
+	//add validation as notmore than 4
+  const numberOfPresignedURLs = req.body.count || 1; // Get the number of URLs from the query parameter (default to 1 if not provided)
+  const presignedURLs = [];
+
+  for (let i = 0; i < numberOfPresignedURLs; i++) {
+    const key = `${req.user._id}/${uuid.v1()}.jpeg`;
+
+    // Generate a presigned URL and add it to the array
+    s3.getSignedUrl('putObject', {
+      Bucket: config.get('bucketName'), // Make it secret also additional security
+      ContentType: 'image/jpeg',
+      Key: key
+    }, (err, url) => {
+      if (!err) {
+        presignedURLs.push({ key, url });
+
+        // Check if we have generated all the required presigned URLs
+        if (presignedURLs.length === numberOfPresignedURLs) {
+          res.send(presignedURLs);
+        }
+      } else {
+        // Handle the error appropriately
+        console.error(err);
+        res.status(500).send('Failed to generate presigned URL');
+      }
+    });
+  }
+});
+
 module.exports = router;
