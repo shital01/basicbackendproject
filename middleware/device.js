cf4c8eed629b773173f8db4d61d27251cae740bc
Sheetal Godara <sheetalgodara@Jaideeps-MacBook-Pro.local>
2024-02-12 17:14:06 +0530

    add a folder

--- middleware/device.js
+++ middleware/device.js
@@ -6,7 +6,7 @@ function device (req,res,next){
 	const deviceId = req.header('deviceId');
 	if(!deviceId) {
 		logger.error("Access denied NO deviceId Provided");
-		return res.status(400).send({code:"1",message:'Access denied NO DeviceId Provided'});
+		return res.status(400).send({code:"No deviceId",message:'Access denied NO DeviceId Provided'});
 	}
 	next()
 }
