dfbc05eb46eef5c412dee35e9383b918d44e70ea
Chandresh Kumar <chandresh.code@gmail.com>
2024-07-16 11:09:08 +0530

    feat: add get api for khata with transactions from both sides

--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,7 @@ const { Transaction } = require('../models/transaction');
 const { Khata } = require('../models/khata');
 
 const authorization = require('../middleware/authorization');
+const { forEach } = require('lodash');
 const dbDebugger = require('debug')('app:db');
 
 //Admin secureity
@@ -260,4 +261,47 @@ router.get('/LastActiveCount', async (req, res) => {
 	]);
 	res.header().send({ error: null, response: counts });
 });
+
+router.get(
+	'/khatasWithTransactionsFromBothUsers',
+	async (req, res) => {
+		const khatas = await Khata.aggregate([
+			{
+				$lookup: {
+					from: 'transactions',
+					localField: '_id',
+					foreignField: 'khataId',
+					as: 'transactions',
+				},
+			},
+		]);
+
+		const khatasWithReducedTransactions = khatas
+			.filter((k) => k.transactions.length > 1)
+			.map((k) => {
+				const transactionSet = new Set();
+				forEach(k.transactions, (transaction) => {
+					transactionSet.add(transaction.userPhoneNumber);
+				});
+				k.senderCount = transactionSet.size;
+				k.senderSet = transactionSet;
+				return k;
+			})
+
+		const khatasWithTransactionsFromBothUsers = khatasWithReducedTransactions
+			.filter((k) => k.senderCount == 2)
+
+		const response = khatasWithTransactionsFromBothUsers.map((k) => {
+			delete k.transactions;
+			return k;
+		});
+
+		res.header().send({
+			error: null,
+			response: response,
+			total: response.length
+		});
+	},
+);
+
 module.exports = router;
