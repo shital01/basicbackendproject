9201e79b0c03524de0d5b012efec3d33b909cbee
Chandresh Kumar <chandresh.code@gmail.com>
2024-07-17 12:31:16 +0530

    feat: add test case for analytics api for khatasWithTransactionsFromBothUsers

--- test/integration/analytics.test.js
+++ test/integration/analytics.test.js
@@ -4,10 +4,20 @@ const { describe, it, beforeEach, afterEach, expect } = require('@jest/globals')
 
 const { User } = require('../../models/user');
 const { Khata } = require('../../models/khata');
+const { Transaction } = require('../../models/transaction');
 
-let server, token;
+let server, token1, token2;
 
-const userid = mongoose.Types.ObjectId();
+const user1 = new User({
+    _id: mongoose.Types.ObjectId(),
+    phoneNumber: '1313131210',
+    name: 'name1',
+});
+const user2 = new User({
+    _id: mongoose.Types.ObjectId(),
+    phoneNumber: '1313131212',
+    name: 'name2',
+})
 
 describe('/api/analytics', () => {
     beforeEach(async () => {
@@ -21,24 +31,95 @@ describe('/api/analytics', () => {
     });
 
     describe('GET /khatasWithTransactionsFromBothUsers', () => {
-        beforeEach(async () => {
-            token = new User({
-				_id: userid,
-				phoneNumber: '1313131212',
-				name: 'name1',
-			}).generateAuthToken();
-        })
-
-        const executeRequest = (token) => {
+        const executeRequest = () => {
             return request(server)
                 .get('/api/analytics/khatasWithTransactionsFromBothUsers')
-                .set('x-auth-token', token)
         }
 
-        it('should return response with 200 status', async () => {
-            
-            const res = await executeRequest(token);
+        it('should return response with 200 status and only one khata', async () => {
+            const khataId1 = mongoose.Types.ObjectId();
+            await Khata.create({
+                _id: khataId1,
+                userName: user1.name,
+                friendName: 'f1',
+                userId: user1._id,
+                friendPhoneNumber: user1.phoneNumber,
+                userPhoneNumber: user2.phoneNumber,
+                interestType: 'N',
+                localId: '1',
+                rotationPeriod: '3M',
+                updatedTimeStamp: 1211231231231,
+                deviceId: '123',
+                settledFlag: true,
+                interest: 0,
+            })
+            await Transaction.create({
+                _id: mongoose.Types.ObjectId(),
+                khataId: khataId1,
+                userId: user1._id,
+                userName: user1.name,
+                userPhoneNumber: user1.phoneNumber,
+                amount: 100,
+                date: 1211231231231,
+                type: 'C',
+            })
+            await Transaction.create({
+                _id: mongoose.Types.ObjectId(),
+                khataId: khataId1,
+                userId: user2._id,
+                userName: user2.name,
+                userPhoneNumber: user2.phoneNumber,
+                amount: 100,
+                date: 1211231231231,
+                type: 'C',
+            })
+            const res = await executeRequest();
             expect(res.status).toBe(200);
+            expect(res.body.total).toBe(1);
+            expect(res.body.response[0].senderCount).toBe(2);
+        })
+
+        it('should return response with 200 status and no khatas', async () => {
+            const khataId1 = mongoose.Types.ObjectId();
+            await Khata.create({
+                _id: khataId1,
+                userName: user1.name,
+                friendName: 'f1',
+                userId: user1._id,
+                friendPhoneNumber: user1.phoneNumber,
+                userPhoneNumber: user2.phoneNumber,
+                interestType: 'N',
+                localId: '1',
+                rotationPeriod: '3M',
+                updatedTimeStamp: 1211231231231,
+                deviceId: '123',
+                settledFlag: true,
+                interest: 0,
+            })
+            await Transaction.create({
+                _id: mongoose.Types.ObjectId(),
+                khataId: khataId1,
+                userId: user1._id,
+                userName: user1.name,
+                userPhoneNumber: user1.phoneNumber,
+                amount: 100,
+                date: 1211231231231,
+                type: 'C',
+            })
+            await Transaction.create({
+                _id: mongoose.Types.ObjectId(),
+                khataId: khataId1,
+                userId: user1._id,
+                userName: user1.name,
+                userPhoneNumber: user1.phoneNumber,
+                amount: 100,
+                date: 1211231231231,
+                type: 'C',
+            })
+            const res = await executeRequest();
+            expect(res.status).toBe(200);
+            expect(res.body.total).toBe(0);
+            expect(res.body.response.length).toBe(0);
         })
     });
 
